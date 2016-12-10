2435b82aaa5eee442c07977346bb4863cf80d3a2
Ismael Medeiros <140083162@aluno.unb.br>
2016-12-10 13:42:21 -0200

    Atualiza arquitetura p2p

--- src/main.cc
+++ src/main.cc
@@ -10,23 +10,23 @@
 
 // Topologia da simulação
 //
-//   Wifi 10.1.5.0
-//                 Ap                  LAN 10.1.4.0
-//  *    *    *    *     10.1.1.0    ================
+//   Wifi 10.1.7.0
+//                 Ap                  LAN 10.1.6.0
+//  *    *    *    *     10.1.3.0    ================
 //  |    |    |    |  ponto a ponto  |    |    |    |
 // n15  n14  n13  n3 -------------- n2   n12  n11  n10
 //                                   |
 //                                   |
-//                                   |       10.1.1.0
+//                                   |       10.1.2.0
 //                                   |----> ponto-a ponto 
-//   Wifi 10.1.2.0                   |
+//   Wifi 10.1.4.0                   |
 //                 Ap                |
 //  *    *    *    *                 |
 //  |    |    |    |    10.1.1.0     |
 // n4   n5   n6   n0 -------------- n1   n7   n8   n9
 //                   ponto-a-ponto   |    |    |    |
 //                                   ================
-//                                     LAN 10.1.3.0
+//                                     LAN 10.1.5.0
 // 
 //
 // Descrição: Simula uma rede de uma empresa que...
@@ -83,8 +83,14 @@ int main(int argc, char *argv[]) {
     pontoAPonto.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     pontoAPonto.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    NetDeviceContainer dispositivosP2P;
-    dispositivosP2P = pontoAPonto.Install(p2pNos);
+    NetDeviceContainer dispositivosP2PUm;
+    dispositivosP2PUm = pontoAPonto.Install(p2pNos.Get(0), p2pNos.Get(1));
+
+    NetDeviceContainer dispositivosP2PDois;
+    dispositivosP2PDois = pontoAPonto.Install(p2pNos.Get(1), p2pNos.Get(2));
+
+    NetDeviceContainer dispositivosP2PTres;
+    dispositivosP2PTres = pontoAPonto.Install(p2pNos.Get(2), p2pNos.Get(3));
 
     NetDeviceContainer dispositivosCSMAUm = criaDispositivoCSMA(p2pNos.Get(1), nCsma, true);
     NetDeviceContainer dispositivosCSMADois = criaDispositivoCSMA(p2pNos.Get(2), nCsma, false);
@@ -108,22 +114,30 @@ int main(int argc, char *argv[]) {
     Ipv4AddressHelper address;
 
     address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer p2pInterfaces;
-    p2pInterfaces = address.Assign(dispositivosP2P);
+    Ipv4InterfaceContainer p2pInterfacesUm;
+    p2pInterfacesUm = address.Assign(dispositivosP2PUm);
 
     address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfacesDois;
+    p2pInterfacesDois = address.Assign(dispositivosP2PDois);
+
+    address.SetBase("10.1.3.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfacesTres;
+    p2pInterfacesTres = address.Assign(dispositivosP2PTres);
+
+    address.SetBase("10.1.4.0", "255.255.255.0");
     address.Assign(dispositivosWifiUm_Sta);
     address.Assign(dispositivosWifiUm_Ap);
 
-    address.SetBase("10.1.3.0", "255.255.255.0");
+    address.SetBase("10.1.5.0", "255.255.255.0");
     Ipv4InterfaceContainer csmaInterfacesUm;
     csmaInterfacesUm = address.Assign(dispositivosCSMAUm);
 
-    address.SetBase("10.1.4.0", "255.255.255.0");
+    address.SetBase("10.1.6.0", "255.255.255.0");
     Ipv4InterfaceContainer csmaIntefacesDois;
     csmaIntefacesDois = address.Assign(dispositivosCSMADois);
 
-    address.SetBase("10.1.5.0", "255.255.255.0");
+    address.SetBase("10.1.7.0", "255.255.255.0");
     address.Assign(dispositivosWifiDois_Sta);
     address.Assign(dispositivosWifiDois_Ap);
 
