21ca627ab293f8c648e6b5a0e6411c9bb5d02671
Ismael Medeiros <140083162@aluno.unb.br>
2016-12-11 00:25:46 -0200

    Código refatorado e comentado

--- src/main.cc
+++ src/main.cc
@@ -15,7 +15,7 @@
 //                 Ap                  LAN 10.1.6.0
 //  *    *    *    *     10.1.3.0    ================
 //  |    |    |    |  ponto a ponto  |    |    |    |
-// n15  n14  n13  n3 -------------- n2   n12  n11  n10
+// n15  n14  n13  n3 -------------- n2   n10  n11  n12
 //                                   |
 //                                   |
 //                                   |       10.1.2.0
@@ -24,35 +24,43 @@
 //                 Ap                |
 //  *    *    *    *                 |
 //  |    |    |    |    10.1.1.0     |
-// n4   n5   n6   n0 -------------- n1   n7   n8   n9
+// n6   n5   n4   n0 -------------- n1   n7   n8   n9
 //                   ponto-a-ponto   |    |    |    |
 //                                   ================
 //                                     LAN 10.1.5.0
 // 
 //
-// Descrição: Simula uma rede de uma empresa que...
+// Descrição: Esse sistema possui duas redes wifi (802.11x) e duas redes Ethernet (802.3).
+// As redes se comunicam por meios dos nós n0, n1, n2, n3.
+// A simulação será feita entre os nós n6 (10.1.4.4) e n15 (10.1.7.4). Onde o nó "n6" é o  
+// receptor e o nó n15 é o transmissor. A simulação está configurada para os nós trocarem 
+// pacotes durante 30s.
 // 
-//
 
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("ProjetoTR1");
 
-CsmaHelper csma;
-YansWifiPhyHelper phy;
+struct CSMAContainer {
+    NodeContainer nodeContainer;
+    NetDeviceContainer deviceContainer;
+    Ipv4InterfaceContainer interfaceContainer;
+};
 
-NodeContainer nosCSMAUm;
-NodeContainer nosCSMADois;
-NodeContainer nosWifiApUm;
-NodeContainer nosWifiStaUm;
-NodeContainer nosWifiApDois;
-NodeContainer nosWifiStaDois;
-NetDeviceContainer deviceAp;
-NetDeviceContainer deviceSta;
+struct WifiContainer {
+    NodeContainer nodeApContainer;
+    NetDeviceContainer deviceApContainer;
+    Ipv4InterfaceContainer interfaceApContainer;
+    
 
-NetDeviceContainer criaDispositivoCSMA(Ptr<Node> no, uint32_t nNosExtras, bool isFirst);
-void criaDispositivoWifi(Ptr<Node> noAp, uint32_t nNosExtras, bool isFirst);
+    NodeContainer nodeStaContainer;
+    NetDeviceContainer deviceStaContainer;
+    Ipv4InterfaceContainer interfaceStaContainer;
+};
+
+CSMAContainer criaDispositivoCSMA(CsmaHelper csma, Ptr<Node> no, uint32_t nNosExtras);
+WifiContainer criaDispositivoWifi(YansWifiPhyHelper phy, Ptr<Node> noAp, uint32_t nNosExtras);
 
 int main(int argc, char *argv[]) {
     bool verbose = true;
@@ -84,100 +92,99 @@ int main(int argc, char *argv[]) {
     pontoAPonto.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     pontoAPonto.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    NetDeviceContainer dispositivosP2PUm;
-    dispositivosP2PUm = pontoAPonto.Install(p2pNos.Get(0), p2pNos.Get(1));
+    /* Cria três dispositivos P2P que serviram para ligar todo sistema */
+    NetDeviceContainer dispositivosP2PUm = pontoAPonto.Install(p2pNos.Get(0), p2pNos.Get(1));
+    NetDeviceContainer dispositivosP2PDois = pontoAPonto.Install(p2pNos.Get(1), p2pNos.Get(2));
+    NetDeviceContainer dispositivosP2PTres = pontoAPonto.Install(p2pNos.Get(2), p2pNos.Get(3));
 
-    NetDeviceContainer dispositivosP2PDois;
-    dispositivosP2PDois = pontoAPonto.Install(p2pNos.Get(1), p2pNos.Get(2));
+    /* Cria dois dispositivos CSMA */
+    CsmaHelper csma;
+    csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+    csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+    CSMAContainer CSMAUm = criaDispositivoCSMA(csma, p2pNos.Get(1), nCsma);
+    CSMAContainer CSMADois = criaDispositivoCSMA(csma, p2pNos.Get(2), nCsma);
 
-    NetDeviceContainer dispositivosP2PTres;
-    dispositivosP2PTres = pontoAPonto.Install(p2pNos.Get(2), p2pNos.Get(3));
+    /* Cria dois dispositivos Wifi */
+    YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
+    WifiContainer WifiUm = criaDispositivoWifi(phy, p2pNos.Get(0), nWifi);
+    WifiContainer WifiDois = criaDispositivoWifi(phy, p2pNos.Get(3), nWifi);
 
-    NetDeviceContainer dispositivosCSMAUm = criaDispositivoCSMA(p2pNos.Get(1), nCsma, true);
-    NetDeviceContainer dispositivosCSMADois = criaDispositivoCSMA(p2pNos.Get(2), nCsma, false);
-
-    criaDispositivoWifi(p2pNos.Get(0), nWifi, true);
-    NetDeviceContainer dispositivosWifiUm_Ap = deviceAp;
-    NetDeviceContainer dispositivosWifiUm_Sta = deviceSta;
-
-    criaDispositivoWifi(p2pNos.Get(3), nWifi, false);
-    NetDeviceContainer dispositivosWifiDois_Ap = deviceAp;
-    NetDeviceContainer dispositivosWifiDois_Sta = deviceSta;
-
+    // Cria um pilha para os protocolos
     InternetStackHelper stack;
-    stack.Install(nosCSMAUm);
-    stack.Install(nosCSMADois);
-    stack.Install(nosWifiApUm);
-    stack.Install(nosWifiStaUm);
-    stack.Install(nosWifiApDois);
-    stack.Install(nosWifiStaDois);
+    stack.Install(CSMAUm.nodeContainer);
+    stack.Install(CSMADois.nodeContainer);
+    stack.Install(WifiUm.nodeApContainer);
+    stack.Install(WifiUm.nodeStaContainer);
+    stack.Install(WifiDois.nodeApContainer);
+    stack.Install(WifiDois.nodeStaContainer);
 
+    // Configura endereços
     Ipv4AddressHelper address;
 
+    // Endereços das conexões ponto-a-ponto
     address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer p2pInterfacesUm;
-    p2pInterfacesUm = address.Assign(dispositivosP2PUm);
-
+    Ipv4InterfaceContainer p2pInterfacesUm = address.Assign(dispositivosP2PUm);
     address.SetBase("10.1.2.0", "255.255.255.0");
-    Ipv4InterfaceContainer p2pInterfacesDois;
-    p2pInterfacesDois = address.Assign(dispositivosP2PDois);
-
+    Ipv4InterfaceContainer p2pInterfacesDois = address.Assign(dispositivosP2PDois);
     address.SetBase("10.1.3.0", "255.255.255.0");
-    Ipv4InterfaceContainer p2pInterfacesTres;
-    p2pInterfacesTres = address.Assign(dispositivosP2PTres);
+    Ipv4InterfaceContainer p2pInterfacesTres = address.Assign(dispositivosP2PTres);
 
+    // Endereço da primeira rede Wifi (Base: 10.1.4.0)
     address.SetBase("10.1.4.0", "255.255.255.0");
-    address.Assign(dispositivosWifiUm_Sta);
-    address.Assign(dispositivosWifiUm_Ap);
+    WifiUm.interfaceApContainer = address.Assign(WifiUm.deviceApContainer);
+    WifiUm.interfaceStaContainer = address.Assign(WifiUm.deviceStaContainer);
 
+    // Endereço da primeira rede CSMA (Base: 10.1.5.0)
     address.SetBase("10.1.5.0", "255.255.255.0");
-    Ipv4InterfaceContainer csmaInterfacesUm;
-    csmaInterfacesUm = address.Assign(dispositivosCSMAUm);
+    CSMAUm.interfaceContainer = address.Assign(CSMAUm.deviceContainer);
 
+    // Endereço da segunda rede CSMA (Base: 10.1.6.0)
     address.SetBase("10.1.6.0", "255.255.255.0");
-    Ipv4InterfaceContainer csmaIntefacesDois;
-    csmaIntefacesDois = address.Assign(dispositivosCSMADois);
+    CSMADois.interfaceContainer = address.Assign(CSMADois.deviceContainer);
 
+    // Endereço da segunda rede Wifi (Base: 10.1.7.0)
     address.SetBase("10.1.7.0", "255.255.255.0");
-    address.Assign(dispositivosWifiDois_Sta);
-    address.Assign(dispositivosWifiDois_Ap);
+    WifiDois.interfaceApContainer = address.Assign(WifiDois.deviceApContainer);
+    WifiDois.interfaceStaContainer = address.Assign(WifiDois.deviceStaContainer);
 
+    // Configura nó receptor
     UdpEchoServerHelper echoServer(9);
-
-    ApplicationContainer serverApps = echoServer.Install(nosCSMAUm.Get(0));
+    ApplicationContainer serverApps = echoServer.Install(WifiUm.nodeStaContainer.Get(nWifi - 1));
     serverApps.Start (Seconds (1.0));
-    serverApps.Stop (Seconds (30.0));
+    serverApps.Stop (Seconds (31.0));
 
-    UdpEchoClientHelper echoClient (csmaInterfacesUm.GetAddress(0), 9);
+    // Configura nó transmissor
+    UdpEchoClientHelper echoClient (WifiUm.interfaceStaContainer.GetAddress(nWifi - 1), 9);
     echoClient.SetAttribute ("MaxPackets", UintegerValue (30));
     echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
     echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
-
-    ApplicationContainer clientApps = echoClient.Install(nosWifiStaDois.Get(nWifi - 1));
+    ApplicationContainer clientApps = echoClient.Install(WifiDois.nodeStaContainer.Get(nWifi - 1));
     clientApps.Start(Seconds(2.0));
     clientApps.Stop(Seconds(31.0));
 
+    // Popula rotas
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
+    // Indica quanto tempo terá a simulação
     Simulator::Stop (Seconds (31.0));
 
     if (tracing) {
         pontoAPonto.EnablePcapAll("main");
-        phy.EnablePcap("main", dispositivosWifiUm_Ap.Get(0));
-        phy.EnablePcap("main", dispositivosWifiDois_Ap.Get(0));
-        csma.EnablePcap("main", dispositivosCSMAUm.Get(0), true);
-        csma.EnablePcap("main", dispositivosCSMADois.Get(0), true);
+        phy.EnablePcap("main", WifiUm.deviceApContainer.Get(0));
+        phy.EnablePcap("main", WifiDois.deviceApContainer.Get(0));
+        csma.EnablePcap("main", CSMAUm.deviceContainer.Get(0), true);
+        csma.EnablePcap("main", CSMADois.deviceContainer.Get(0), true);
 
-        AnimationInterface::SetConstantPosition(nosWifiApUm.Get(0), 2, 2);
-        AnimationInterface::SetConstantPosition(nosWifiApDois.Get(0), 2, 7);
-        AnimationInterface::SetConstantPosition(nosCSMAUm.Get(0), 4, 2);
-        AnimationInterface::SetConstantPosition(nosCSMAUm.Get(1), 5, 2);
-        AnimationInterface::SetConstantPosition(nosCSMAUm.Get(2), 6, 2);
-        AnimationInterface::SetConstantPosition(nosCSMAUm.Get(3), 7, 2);
-        AnimationInterface::SetConstantPosition(nosCSMADois.Get(0), 4, 7);
-        AnimationInterface::SetConstantPosition(nosCSMADois.Get(1), 5, 7);
-        AnimationInterface::SetConstantPosition(nosCSMADois.Get(2), 6, 7);
-        AnimationInterface::SetConstantPosition(nosCSMADois.Get(3), 7, 7);
+        AnimationInterface::SetConstantPosition(WifiUm.nodeApContainer.Get(0), 2, 2);
+        AnimationInterface::SetConstantPosition(WifiDois.nodeApContainer.Get(0), 2, 7);
+        AnimationInterface::SetConstantPosition(CSMAUm.nodeContainer.Get(0), 4, 2);
+        AnimationInterface::SetConstantPosition(CSMAUm.nodeContainer.Get(1), 5, 2);
+        AnimationInterface::SetConstantPosition(CSMAUm.nodeContainer.Get(2), 6, 2);
+        AnimationInterface::SetConstantPosition(CSMAUm.nodeContainer.Get(3), 7, 2);
+        AnimationInterface::SetConstantPosition(CSMADois.nodeContainer.Get(0), 4, 7);
+        AnimationInterface::SetConstantPosition(CSMADois.nodeContainer.Get(1), 5, 7);
+        AnimationInterface::SetConstantPosition(CSMADois.nodeContainer.Get(2), 6, 7);
+        AnimationInterface::SetConstantPosition(CSMADois.nodeContainer.Get(3), 7, 7);
 
         AnimationInterface anim ("animation.xml");
     }
@@ -190,61 +197,63 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-NetDeviceContainer criaDispositivoCSMA(Ptr<Node> no, uint32_t nNosExtras, bool isFirst) {
+// Essa função irá criar um dispositivo CSMA e irá retornar um container com esse
+// dispositivo e os nós correspondentes.
+CSMAContainer criaDispositivoCSMA(CsmaHelper csma, Ptr<Node> noP2P, uint32_t quantNosExtras) {
+
+    // Cria os nós
     NodeContainer nosCSMA;
-    nosCSMA.Add(no);
-    nosCSMA.Create(nNosExtras);
+    nosCSMA.Add(noP2P);
+    nosCSMA.Create(quantNosExtras);
 
-    if (isFirst) {
-        nosCSMAUm = nosCSMA;
-    } else {
-        nosCSMADois = nosCSMA;
-    }
+    // Armazena os nós no container
+    CSMAContainer csmaContainer;
+    csmaContainer.nodeContainer = nosCSMA;
 
-    csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
-    csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+    // Armazena no container o novo dispositivo CSMA
+    csmaContainer.deviceContainer = csma.Install(nosCSMA);
 
-    return csma.Install(nosCSMA);
+    return csmaContainer;
 }
 
-void criaDispositivoWifi(Ptr<Node> noAp, uint32_t nNosExtras, bool isFirst) {
-    NodeContainer nosWifiSta;
-    nosWifiSta.Create(nNosExtras);
-    NodeContainer noWifiAp = noAp;
+// Essa função irá criar dois dispositivos Wifi (Ap e Sta) e irá retornar um container
+// com esses dispositivos e os nós correspondentes.
+WifiContainer criaDispositivoWifi(YansWifiPhyHelper phy, Ptr<Node> noAp, uint32_t nNosExtras) {
 
-    if (isFirst) {
-        nosWifiStaUm = nosWifiSta;
-        nosWifiApUm = noAp;
-    } else {
-        nosWifiStaDois = nosWifiSta;
-        nosWifiApDois = noAp;
-    }
+    // Cria os nós
+    NodeContainer wifiSta;
+    wifiSta.Create(nNosExtras);
+
+    // Armazena os nós no container
+    WifiContainer wifiContainer;
+    wifiContainer.nodeApContainer = noAp;
+    wifiContainer.nodeStaContainer = wifiSta;
     
-
+    // Configura ChannelHelpers e o PHY
     YansWifiChannelHelper canal = YansWifiChannelHelper::Default();
-    phy = YansWifiPhyHelper::Default();
     phy.SetChannel(canal.Create());
-
+    
+    // Configura MAC
     WifiHelper wifi;
     wifi.SetRemoteStationManager("ns3::AarfWifiManager");
-
     WifiMacHelper mac;
+
+    // Configura o SSID da infraestrutura da rede
     Ssid ssid = Ssid ("ns-3-ssid");
+
+    // Armazena no container o novo dispositivo Wifi Ap
+    mac.SetType ("ns3::ApWifiMac",
+                    "Ssid", SsidValue (ssid));
+    wifiContainer.deviceApContainer = wifi.Install (phy, mac, noAp);
+
+    // Armazena no container os novos dispositivos Wifi STA
     mac.SetType ("ns3::StaWifiMac",
             "Ssid", SsidValue (ssid),
             "ActiveProbing", BooleanValue (false));
-
-    NetDeviceContainer staDevices;
-    staDevices = wifi.Install (phy, mac, nosWifiSta);
-
-    mac.SetType ("ns3::ApWifiMac",
-                    "Ssid", SsidValue (ssid));
-
-    NetDeviceContainer apDevices;
-    apDevices = wifi.Install (phy, mac, noWifiAp);
+    wifiContainer.deviceStaContainer = wifi.Install (phy, mac, wifiSta);
     
+    // Configura mobilidade geral dos nós
     MobilityHelper mobility;
-
     mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
                                     "MinX", DoubleValue (0.0),
                                     "MinY", DoubleValue (0.0),
@@ -252,14 +261,15 @@ void criaDispositivoWifi(Ptr<Node> noAp, uint32_t nNosExtras, bool isFirst) {
                                     "DeltaY", DoubleValue (10.0),
                                     "GridWidth", UintegerValue (3),
                                     "LayoutType", StringValue ("RowFirst"));
-
+    
+    // Configura nó Ap para ter não ter mobilidade, ou seja, esteja estático em sua posição
+    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    mobility.Install (wifiContainer.nodeApContainer);
+    
+    // Configura nós Sta para ter mobilidade randômica
     mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
                                 "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
-    mobility.Install (nosWifiSta);
+    mobility.Install (wifiContainer.nodeStaContainer);    
 
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-    mobility.Install (noWifiAp);
-
-    deviceAp = apDevices;
-    deviceSta = staDevices;   
+    return wifiContainer;
 }
